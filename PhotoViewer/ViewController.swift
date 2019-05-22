85cd0d72d7f4c630f649a0cb85182663be729d1a
Soumi <soudut3@gmail.com>
2019-05-22 20:28:13 +0530

    Initial Commit

---
+++ PhotoViewer/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  PhotoViewer
+//
+//  Created by Dutta, Soumitra on 22/05/19.
+//  Copyright © 2019 Dutta, Soumitra. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
